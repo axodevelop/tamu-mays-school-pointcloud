7e9b99d3da37c25a294abecad44702515af6fc6a
Edinson Erraez <erraez.edinson@gmail.com>
2021-07-28 17:39:58 -0500

    build

---
+++ pointclouds/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 29321395,
+    "boundingBox": {
+        "lx": 2473122.3229370119,
+        "ly": 11119587.634887696,
+        "lz": 88.01200866699219,
+        "ux": 2474350.1854248049,
+        "uy": 11120815.497375489,
+        "uz": 1315.874496459961
+    },
+    "tightBoundingBox": {
+        "lx": 2473122.323,
+        "ly": 11119587.635,
+        "lz": 88.012,
+        "ux": 2474206.606,
+        "uy": 11120815.497,
+        "uz": 195.292
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 10.633601188659668,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
